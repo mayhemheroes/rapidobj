5e63a9de2c61af246914de587266e8f38a420957
xansec <76011430+xansec@users.noreply.github.com>
2023-04-26 13:34:51 -0700

    Merge branch 'guybrush77:master' into master

---
+++ fuzz/rapidobj-fuzz.cpp
@@ -0,0 +1,18 @@
+#define NDEBUG
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/rapidobj/rapidobj.hpp"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+    std::ofstream binFile("tmp.bin", std::ios::out | std::ios::binary);
+    binFile.write((char*)Data, Size);
+    binFile.close();
+
+    rapidobj::ParseFile("tmp.bin");
+
+    return 0;
+}
